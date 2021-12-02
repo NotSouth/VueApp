bb11b01dd70424e40eea9ef1e83a78190f85c110
tyjuji <tyjuji@gmx.com>
2021-12-02 15:47:07 +0100

    Live readings work

--- index.js
+++ index.js
@@ -1,78 +1,120 @@
 //const baseUri = "http://localhost:3000/cars/"
-const baseUri ="https://localhost:10010/api/air/"
+//const baseUri = "https://localhost:5001/api/air"
+const baseUri = "https://notsouthair.azurewebsites.net/api/air"
+
 Vue.createApp({
     data() {
         return {
-            dataList:[],
-            currentData: null,
+            dataList: [],
+            latest: {
+                "id": 0,
+                "cO2": 0,
+                "temperature": 0,
+                "humidity": 0
+                },
             error: null,
-            statuscode:null,
+            statuscode: null,
+            timer: null
         }
     },
     created() {
         // created() is a life cycle method, not an ordinary method
         // created() is called automatically when the page is loaded
         console.log("created method called")
-        this.getAll()
+        //this.getAll()
+        this.getLatest()
+        this.timer = setInterval(this.autoUpdate, 5000);
+    },
+    beforeDestroy() {
+        this.cancelAutoUpdate();
     },
     methods: {
+        cancelAutoUpdate() {
+            clearInterval(this.timer);
+        },
+        autoUpdate() {
+            //this.getAll();
+            this.getLatest();
+        },
         cleanList() {
             this.dataList = [];
             this.error = null;
             console.log("count data: " + this.dataList.length);
         },
         //Read this for an example: https://vuejs.org/v2/cookbook/using-axios-to-consume-apis.html
-         getAll() {
-             //axios call that returns all the elements from the webservice
+        getAll() {
+            //axios call that returns all the elements from the webservice
             axios.get(baseUri)
-            .then(response => {
-             var divtag = document.getElementById("content"); //What's the point?
+                .then(response => {
+                    var divtag = document.getElementById("content"); //What's the point?
 
-             console.log("in function getAll");
-             console.log("status code: "+ response.status );
+                    console.log("in function getAll");
+                    console.log("status code: " + response.status);
 
-             //add the returning data from the webservice to the variable dataList
-             this.dataList = response.data;
-             this.status = response.status;
-              
-             console.log("length of the dataList array " + this.dataList.length)
+                    //add the returning data from the webservice to the variable dataList
+                    this.dataList = response.data;
+                    this.status = response.status;
+
+                    console.log("length of the dataList array " + this.dataList.length)
 
 
-            })
-            .catch(error = (ex) => {
-                //resultElement.innerHTML = generateErrorHTMLOutput(error);
-                this.dataList = []
-                this.error = ex.message
-                console.log("Error:" + this.error);
-            })      
-            
+                })
+                .catch(error = (ex) => {
+                    //resultElement.innerHTML = generateErrorHTMLOutput(error);
+                    this.dataList = []
+                    this.error = ex.message
+                    console.log("Error:" + this.error);
+                })
+
         },
-        getById(id){
+        getById(id) {
             //axios call that returns the items from a specified user 
-            axios.get(baseUri + "/"  + id)
-            .then(response => {
-            
-            console.log("URI: " + baseUri + "/" +id)
+            axios.get(baseUri + "/" + id)
+                .then(response => {
 
-             console.log("in function getById");
-             console.log("status code: "+ response.status );
+                    console.log("URI: " + baseUri + "/" + id)
 
-             //add the returning data from the webservice to the variable posts
-             this.dataList = response.data;
-             this.status = response.status;
-              
-             console.log("length of the dataList array " + this.dataList.length)
-            })
-            .catch(error = (ex) => {
-              this.dataList = []
-              this.error = ex.message
-              console.log("Error:" + this.error);
-            })      
+                    console.log("in function getById");
+                    console.log("status code: " + response.status);
+
+                    //add the returning data from the webservice to the variable posts
+                    this.dataList = response.data;
+                    this.status = response.status;
+
+                    console.log("length of the dataList array " + this.dataList.length)
+                })
+                .catch(error = (ex) => {
+                    this.dataList = []
+                    this.error = ex.message
+                    console.log("Error:" + this.error);
+                })
+        },
+        getLatest() {
+            //Latest
+            axios.get(baseUri + "/Latest")
+                .then(response => {
+
+                    console.log("URI: " + baseUri + "/Latest")
+
+                    console.log("in function getLatest");
+                    console.log("status code: " + response.status);
+
+                    //add the returning data from the webservice to the variable posts
+                    this.latest = response.data;
+                    this.status = response.status;
+
+                    console.log("length of the dataList array " + this.dataList.length)
+                })
+                .catch(error = (ex) => {
+                    this.dataList = []
+                    this.error = ex.message
+                    console.log("Error:" + this.error);
+                })
         },
         // Post(){
         //     axios.post(baseUri,{"id":this.Id,"vendor":this.Vendor,"model":this.Model,"price":this.Price})
         //     .then(response => {
-            
+
         //     console.log("URI: ")
 
         //      console.log("in post s");
@@ -81,7 +123,7 @@ Vue.createApp({
         //      //add the returning data from the webservice to the variable posts
         //      this.dataList = response.data;
         //      this.status = response.status;
-              
+
         //      console.log("length of s array " + this.dataList.length)
         //     })
         //     .catch(error = (ex) => {
@@ -94,7 +136,7 @@ Vue.createApp({
         //     //axios call that returns the items from a specified user 
         //     axios.delete(baseUri + "/"  + id)
         //     .then(response => {
-            
+
         //     console.log("URI: " + baseUri + "?id=" +id)
 
         //      console.log("in function getById");
@@ -103,7 +145,7 @@ Vue.createApp({
         //      //add the returning data from the webservice to the variable posts
         //      this.dataList = response.data;
         //      this.status = response.status;
-              
+
         //      console.log("length of the dataList array " + this.dataList.length)
         //     })
         //     .catch(error = (ex) => {
@@ -112,8 +154,5 @@ Vue.createApp({
         //       console.log("Error:" + this.error);
         //     })      
         // }
-        
-       
-       
     }
 }).mount("#app")
